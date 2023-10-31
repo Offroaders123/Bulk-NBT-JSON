846c5bdc97fc24d8cef708a45c7450631f96451c
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-10-31 03:36:08 -0700

    Named Imports (vs Namespaces)
    Also, some modern formatting from how I've been doing things for a while now (the last year or so).
    
    Made the script installable as a global shell command! I think that was the initial goal for this, I just didn't know how to do it yet. I want to streamline this so you can use it as a normal CLI command, rather than an interface that you 'go through menus' in a way, since that's currently how it works still.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import { promises as fs } from "fs";
-import path from "path";
-import readline from "readline";
+#!/usr/bin/env node
+
+import { readFile, writeFile, mkdir, readdir, stat } from "node:fs/promises";
+import { join, basename, extname } from "node:path";
+import { createInterface } from "node:readline";
 import * as NBT from "nbtify";
 
-const input = readline.createInterface({ input: process.stdin, output: process.stdout });
+const input = createInterface({ input: process.stdin, output: process.stdout });
 input.on("close",() => process.exit(0));
 
 /**
@@ -14,6 +16,7 @@ input.on("close",() => process.exit(0));
 async function prompt(query,fallback,values = []){
   /** @type {string} */
   let response = await new Promise(resolve => input.question(query,resolve));
+
   if (response === "" || values.length && !values.includes(response)){
     if (fallback === undefined) fallback = values[0];
     response = fallback;
@@ -21,6 +24,7 @@ async function prompt(query,fallback,values = []){
     process.stdout.clearLine(1);
     console.log(`${query}${fallback}`);
   }
+
   return response;
 }
 
@@ -46,34 +50,37 @@ const sourceDir = await prompt("> ",method ? "./test/nbt_snbt" : "./test/nbt");
 const mirrorDir = `${sourceDir}_${conversions[method].inverse}`;
 
 try {
-  await fs.stat(sourceDir);
+  await stat(sourceDir);
 } catch (error){
   console.error(`\nCouldn't find directory "${sourceDir}".`);
   input.close();
 }
 
-await fs.mkdir(mirrorDir,{ recursive: true });
+await mkdir(mirrorDir,{ recursive: true });
 
 console.log(`\nCreated directory for ${method ? "re-compiled" : "mirrored"} ${conversions[method].inverse.toUpperCase()} files at "${mirrorDir}"`);
 console.log(`Opening "${sourceDir}" to parse the source NBT...\n`);
 
-const entries = await fs.readdir(sourceDir,{ withFileTypes: true });
+const entries = await readdir(sourceDir,{ withFileTypes: true });
 
 for (const entry of entries){
   const kind = entry.isDirectory() ? "directory" : "file";
   if (kind === "directory") continue;
+
   const { name } = entry;
-  const extension = path.extname(name);
+  const extension = extname(name);
   if (extension !== `.${conversions[method].kind}`) continue;
 
   console.log(`Converting "${name}"...`);
+
   /** @type {string | Uint8Array} */
-  let data = await fs.readFile(path.join(sourceDir,name),{ encoding: method ? "utf8" : null });
+  let data = await readFile(join(sourceDir,name),{ encoding: method ? "utf8" : null });
   data = method ? await NBT.write(NBT.parse(/** @type {string} */ (data))) : NBT.stringify(await NBT.read(/** @type {Uint8Array} */ (data)),{ space: 2 });
 
-  const mirrorName = `${path.basename(name,extension)}.${conversions[method].inverse}`;
+  const mirrorName = `${basename(name,extension)}.${conversions[method].inverse}`;
   console.log(`Saving "${mirrorName}"...\n`);
-  await fs.writeFile(path.join(mirrorDir,mirrorName),data);
+
+  await writeFile(join(mirrorDir,mirrorName),data);
 }
 
 input.close();
--- test/index.js
+++ test/index.js
@@ -1,11 +1,11 @@
-import { promises as fs } from "fs";
+import { readFile } from "node:fs/promises";
 import * as NBT from "nbtify";
 
-let data = await fs.readFile("./nbt/hello_world.nbt");
-console.log(data);
+const data = await readFile(new URL("./nbt/hello_world.nbt",import.meta.url));
+console.log(data,"\n");
 
-let content = await NBT.read(data);
-console.log(content);
+const result = await NBT.read(data);
+console.log(result,"\n");
 
-let result = await NBT.write(content);
-console.log(result);
+const recompile = await NBT.write(result);
+console.log(Buffer.from(recompile));
